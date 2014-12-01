a9f79cfdeaccf9302833559d1a1decfbba61b5f3
Crispin226 <Crispin226@users.noreply.github.com>
2014-12-01 12:34:23 -0500

    Update OrderedSuperArray.java

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -1,8 +1,22 @@
-public OrderedSuperArray{
+public class OrderedSuperArray extends SuperArray{
+private int StorageNum;
+private String[] Array1;
+public SuperArray(){
+StorageNum = 10;
+Array1 = new String[]{"Bob", " Dylan","you",};
+}
+public SuperArray(int size){
+StorageNum = size;
+Array1 = new String[size];
+}
+public SuperArray(String[] e){
+Array1 = e;
+}
+
+public void add(String e){
+for (int i = 0; i, Array1.size(); i ++){
+ if e.compareTo(Array1[i] > 0) &&
 
 
-
-
-
-
+}
 }
