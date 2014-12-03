df31a6dfa198810cb0b21d3cc68b81560343cc90
Crispin226 <Crispin226@users.noreply.github.com>
2014-12-02 19:49:55 -0500

    Update Driver.java

--- 03WordGrid/Driver.java
+++ 03WordGrid/Driver.java
@@ -1,13 +1,27 @@
 public class Driver{
+
 public static void main(String[] args) {
-if (args.length != 4 && args.length != 5) {
-System.out.println("Command Input as Follows");
-System.out.println("java WordGrid <rows> <cols> <filename> <filled> [seed]");
-return;
-}
-try {
-WordGrid gridInitial = new WordGrid(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-if (args.length == 5) {
-gridInitial.setSeed(Integer.parseInt(args[4]));
+    if (args.length != 3 && args.length != 4) {
+	System.out.println("Arguments are as follows:")
+	  System.out.println("java WordGrid <rows> <cols> <seed> <filled>");
+	return;
+    }
+    try {
+	WordGrid w = new WordGrid(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+	w.setSeed(Integer.parseInt(args[2]));
+	if (args[3].equals("0")) {
+	    w.loadFromFile("words.txt", false);
+	} else {
+	    w.loadFromFile("words.txt", true);
+	}
+	System.out.println("Find these words");
+	System.out.println(w.wordsInPuzzle());
+	System.out.println(w.toString);
+    }
+    catch (Exception e) {
+	  System.out.println("Arguments are as follows:")
+	  System.out.println("java WordGrid <rows> <cols> <seed> <filled>");
+	e.printStackTrace();
+    }
 }
 }
