08effc8325d8030aa528a7036ca7cf151e540261
Crispin226 <benlanier24@gmail.com>
2014-12-07 19:58:25 -0500

    Small modifications

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -1,42 +1,35 @@
 public class OrderedSuperArray extends SuperArray{
-private int StorageNum;
-private String[] Array1;
-public SuperArray(){
-StorageNum = 10;
-Array1 = new String[]{"Bob", " Dylan","you",};
-}
-public SuperArray(int size){
-StorageNum = size;
-Array1 = new String[size];
-}
-public SuperArray(String[] e){
-Array1 = e;
-}
-
-public void add(String e){
-for (int i = 0; i, Array1.size(); i ++){
- if e.compareTo(Array1[i] > 0) &&
- public void InsertionSort(){
-    OrderedSuperArray c = new OrderedSuperArray();
-    String b;
-    for(int i = 0; i < this.size(); i++){
-	If (!(c[i] < c[i +1]) && ([i] > c[i-1])){
-	    b =  c.remove(i);
-	    for(int z = 0; z < this.size(); z++){
-		If  ((c[z]  < c[z +1]) && (c[z] > c[z-1])){
-		    c.add(b);
-			    
-			}
-	    }
-		
-		
-
-	    
-
-	}
-
-
+    private int StorageNum;
+    private String[] Array1;
+    public SuperArray(){
+	StorageNum = 10;
+	Array1 = new String[]{"Bob", " Dylan","you",};
     }
+    public SuperArray(int size){
+    StorageNum = size;
+    Array1 = new String[size];
+    }
+    public SuperArray(String[] e){
+	Array1 = e;
+    }
+
+    public void add(String e){
+	for (int i = 0; i, Array1.size(); i ++){
+	    if (e.compareTo(Array1[i] > 0) &&  public void InsertionSort(){
+		    OrderedSuperArray c = new OrderedSuperArray();
+		    String b;
+		    for(int i = 0; i < this.size(); i++){
+			If (!(c[i] < c[i +1]) && ([i] > c[i-1])){
+			    b =  c.remove(i);
+			    for(int z = 0; z < this.size(); z++){
+				If  ((c[z]  < c[z +1]) && (c[z] > c[z-1])){
+				    c.add(b);
+			    
+				}
+			    }
+			}}}}}
+		
+
 	return c;
 }
 public int find(String target){
