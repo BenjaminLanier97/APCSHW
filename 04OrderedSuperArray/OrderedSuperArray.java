7a5fc77272fa0cd2054eb314a929bf76031ddae6
Crispin226 <Crispin226@users.noreply.github.com>
2014-12-15 05:26:19 -0500

    Update OrderedSuperArray.java

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -1,48 +1,57 @@
 public class OrderedSuperArray extends SuperArray{
     private int StorageNum;
     private String[] Array1;
-    SuperArray(){
+    OrderedSuperArray(){
 	this.StorageNum = 10;
 	Array1 = new String[]{"Bob", " Dylan","you",};
     }
-    SuperArray(int size){
+    OrderedSuperArray(int size){
     this.StorageNum = size;
     this.Array1 = new String[size];
     }
-    SuperArray(String[] e){
+    OrderedSuperArray(String[] e){
 	this.Array1 = e;
     }
 
    public void add(String e){
-	for (int i = 0; i< Array1.size(); i ++){
+	for (int i = 0; i< Array1.length; i ++){
 	    if (e.compareTo(Array1[i]) > 0) {
 		e.add(i);
 	    }
 	}
     }
 
+public static void main(String[] args){
+OrderedSuperArray Test1 = new OrderedSuperArray();
+super.out(Test1.toString());
+Test1.add("Joe");
+super.out(Test1.toString());
+Test1.Resize(5);
+super.out(Test1.toString());
+Test1.add("Crispin");
+super.out(Test1.toString());
+Test1.add("Harold");
+super.out(Test1.toString());
+}
+
+
   public void InsertionSort(){
-		    OrderedSuperArray c = new OrderedSuperArray();
-		    String b;
-		    for(int i = 0; i < this.size(); i++){
-			if (!(c[i] < c[i +1]) && (c[i] > c[i-1])){
-			    b =  c.remove(i);
-			    for(int z = 0; z < this.size(); z++){
-				if ((c[z]  < c[z +1]) && (c[z] > c[z-1])){
-				    c.add(b);
+      String b;
+		  for(int i = 0; i < Array1.length; i++){
+			if (!(Array1[i] < Array1[i+1]) && (Array1[i] > Array1[i-1])){
+			    b =  Array1.remove(i);
+			    for(int z = 0; z < Array1.length; z++){
+				if ((Array1[z]  < Array1[z +1]) && (Array1[z] > Array1[z-1])){
+				    Array1.add(b);
 			    
 				
 			    
 				}}}}
-		
-
-	return c;
 }
 
 public int find(String target){
-    OrderedSuperArray c = new OrderedSuperArray();
-     for(int i = 0; i < this.size(); i++){
-	 if (c[i].equals(target)) {
+     for(int i = 0; i < Array1.length; i++){
+	 if (Array1[i].equals(target)) {
 	     return i;
 	 }
 	 else{
@@ -51,13 +60,19 @@ public int find(String target){
 	 }}}
 
 public void SelectionSort(){
-    String MinValue;
+    int MinValue;
+    int Substitue;
+    MinValue = Array1[i];
     
-    for (int i = 0; i <SuperArray.size(); i++){
-	 MinValue = superArray[i];
-	 for (int j = 0; j <SuperArray.size(); j++){
-	     if (SuperArray[i] < SuperArray[j]){
-		 SuperArray[i] = SuperArray [j];
+    for (int i = 0; i <Array1.length; i++){
+	  MinValue = Array1[i];
+	 for (int j = i; j <Array1.length; j++){
+	     if (Array1[j]< MinValue){
+		 MinValue = Array1[j];
+		 
 		     }
-	     MinValue = SuperArray[j];
-	 }}}}
+	 }
+	 Substitute = Array1[i];
+	      Array1[i] =MinValue;
+	    Array1[j] = Substitute;
+    }}}
