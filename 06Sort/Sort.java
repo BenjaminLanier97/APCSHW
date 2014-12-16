d71df43d88a8149298aee4bfa7b09d0ec5f29460
Crispin226 <Crispin226@users.noreply.github.com>
2014-12-16 12:35:52 -0500

    Create Sort.java

---
+++ 06Sort/Sort.java
@@ -0,0 +1,10 @@
+public class BubbleSort{
+
+    public void BubbleSort(){
+	int temp = 0;
+	for (int i = 0;i <List.length;i++){
+	    for (int j = i; j < List.length;j++){
+		if (List[i] > List[j]){
+		    temp = List[i];
+		    List[i] = List[j];
+		}}}}}
